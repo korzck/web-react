ea8e59b9fc60b0926d9c7d22b0a71b1d68dea61e
konstantin.koretsk <konstantin.koretsk@aliexpress.ru>
2024-01-13 19:44:47 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  base: "https://github.com/korzck/web-react",
+  base: "web-react",
   plugins: [react()],
 })

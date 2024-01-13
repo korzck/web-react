f07e415dfabd1b69c34d52c214ca8c1048696378
konstantin.koretsk <konstantin.koretsk@aliexpress.ru>
2024-01-13 19:11:17 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  // base: "https://github.com/korzck/web-react",
+  base: "https://github.com/korzck/web-react",
   plugins: [react()],
 })
